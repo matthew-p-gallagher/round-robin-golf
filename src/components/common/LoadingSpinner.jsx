0876564474fe007f08e39666b09bf298576cd81f
Matthew Gallagher <mg95@hotmail.co.uk>
2025-12-04 23:25:17 +0000

    Fix loading spinner

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -4,11 +4,10 @@
  * @param {Object} props
  * @param {string} [props.message] - Loading message to display (default: "Loading...")
  */
-export default function LoadingSpinner({ message = 'Loading...' }) {
+export default function LoadingSpinner() {
   return (
     <div className="loading-container">
       <div className="loading-spinner"></div>
-      <p>{message}</p>
     </div>
   )
 }
